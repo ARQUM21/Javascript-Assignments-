05da90a2336610af369ab380184ed93f406868c1
ARQUM21 <154671211+ARQUM21@users.noreply.github.com>
2024-02-29 12:23:16 +0500

    chapter 1 and 2 done

---
+++ Chapter1/app.js
@@ -0,0 +1,14 @@
+
+
+alert("hello world")
+
+alert("Error! Please enter a valid password.")
+
+alert("Welcome to JS land... \nHappy Coding! ")
+
+alert("Welcome to Jsland..")
+
+
+alert('Hello I can run JS though my web broswer,s console')
+
+
---
+++ Chapter2/app.js
@@ -0,0 +1,17 @@
+var username = "arqum"
+
+alert (username)
+
+var myName = "m.arqum tariq"
+
+alert(myName)
+
+alert("pizza\npizz\npiz\npi\np")
+
+var email = "marqum24@gmail.com"
+
+alert(email)
+
+var book = "A samter way to learn javscript"
+
+alert(book)
