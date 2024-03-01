739a177a09e31c5fc4b9b300ef52f42e358fcf57
ARQUM21 <154671211+ARQUM21@users.noreply.github.com>
2024-03-01 16:54:58 +0500

    Arithmetic Operator Done

---
+++ Arithmetic Operators/app.js
@@ -0,0 +1,133 @@
+
+//Q1
+
+var x = 25
+var y = 7
+
+console.log(x % y)
+
+
+//Q2
+
+var a = 15
+var b = 4
+
+console.log(a * b)
+
+var a = 10
+var b = 2
+
+console.log(a - b)
+
+
+//Q3
+
+var c = 12
+var d = 3
+
+console.log(c  ** d)
+
+
+//Q4
+
+var e = 10
+var f = 3
+
+console.log(e =+ f * 2)
+
+//Q5
+
+var g = 20
+var h = 4
+
+console.log(g / h)
+
+var g = 25
+var h = 4
+
+console.log(g / h)
+
+
+//Q6
+
+var i = 8
+var j = 3
+
+console.log(i * j)
+
+console.log(i * 2)
+
+
+//Q7
+
+var k = 18
+var l = 5
+
+console.log(k - 1 + 2)
+
+
+//Q8
+
+var m = 7
+var n = 2
+
+console.log(m ** n)
+
+
+//Q9
+
+var o = 13
+var p = 4
+
+console.log(o % (p + 2))
+
+
+//Q10
+
+var q = 16
+var r = 3
+
+console.log(q * r)
+console.log(q / 2)
+
+
+//Q11
+
+var s = 25
+var t = 3
+
+console.log(s / t)
+console.log(1 - s)
+
+
+//Q12
+
+var u = 11
+var v = 3
+
+console.log(u - v)
+console.log(v + 2)
+
+
+//Q13
+
+var w = 22
+var x = 5
+
+console.log(w + (x **2))
+
+
+//Q14
+
+var y = 19
+var z = 4
+
+console.log(y % (z + 1))
+
+
+//Q15
+
+var a = 10
+var b = 3
+
+console.log(a * (b - 2))
