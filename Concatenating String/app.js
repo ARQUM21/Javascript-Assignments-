7a2defcab4b78578758b5ea95386b690a17f5a2a
ARQUM21 <154671211+ARQUM21@users.noreply.github.com>
2024-03-02 13:01:01 +0500

    Concatentating Strings

---
+++ Concatenating String/app.js
@@ -0,0 +1,40 @@
+
+
+var firstName = 'Arqum'
+var lastName = 'Tariq'
+
+var fullName = firstName + ' ' + lastName
+
+// document.write('<h1>' + 'Salaam ' + fullName  + '</h1>')
+document.write(`<h1> Salaam ${fullName} </h1>`)
+
+
+// Template Strings 
+// ye use krte hen  Back-Tics ``
+
+// var fName = `Salaam ${firstName} ${lastName}`
+
+var tableNumber = 4
+var tNum = 1
+var tdNum = 1
+
+document.write(`<h5> ${tableNumber} Ka Table </h5>`)
+document.write(`<li> ${tableNumber} x ${tdNum} =  ${tableNumber * tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+document.write(`<li> ${tableNumber} x ${++tdNum} =  ${tableNumber * ++tNum} </li>`)
+
+// var tableNumber = 4
+// var tNum  = 11
+// var tdNum = 11
+
+// document.write('<h5> ${tableNumber} ka table </h5>')
+// document.write('<li> ${tablenumber} * ${tdNum} = ${tableNumbber * tNum} <li>')
+// document,write('<li> ${tableNumber} * ${++tdnum} = ${tableNumber * ++tNum} <li> ')
+// document.write('<li> ${tableNumber} * ${++tdNum} = ${tableNumber * ++tNum <li>')
