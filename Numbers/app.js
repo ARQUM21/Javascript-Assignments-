396d66d433e098b5c15ab30d0a2d927cd000beed
ARQUM21 <154671211+ARQUM21@users.noreply.github.com>
2024-05-04 00:10:43 +0500

    Password generator

--- Numbers/app.js
+++ Numbers/app.js
@@ -32,9 +32,69 @@
 
 
 
-var heading = document.getElementById("random");
-function generateOTPNumber() {
+// var heading = document.getElementById("random");
+// function generateOTPNumber() {
     
-    var random = Math.round(Math.random() * 10000);
-    heading.innerText = random;
+//     var random = Math.round(Math.random() * 10000);
+//     heading.innerText = random;
+// }
+
+
+// var string = "ABCDEF1234567sqwrtyuiopsghjklzxvnm";
+// var password_h1 = document.getElementById("password");
+
+// var password = "";
+
+// function generatepassword(){
+//     password = "";
+//     for(var i = 0 ; i < 8 ; i++){
+//         var randomNumber = Math.round(Math.random() * string.length);
+//         console.log(randomNumber, string.charAt(randomNumber));
+//         password = password + string.charAt(randomNumber);
+//         console.log(password);
+//     }
+//     password_h1.innerText = password;
+// }
+
+
+
+//dice//
+
+
+var user_number = document.getElementById("user_number");
+var random_number = document.getElementById("random_number");
+var result = document.getElementById("result");
+var info_box_result = document.getElementById("info_box_result");
+var total = document.getElementById("total");
+var win_html = document.getElementById("win");
+var lose_html = document.getElementById("lose");
+var msg = document.getElementById("msg");
+
+
+var count = 10;
+var win = 0;
+var loss = 0;
+
+function trymyluck(userNumber){
+  var randomNumber = Math.random() * 6;
+  var ceil = Math.ceil(randomNumber);
+  console.log(ceil);
+  user_number.innerText = userNumber;
+  random_number.innerText = ceil;
+  total.innerText = --count;
+
+  if(ceil === userNumber){
+    console.log("userwin");
+    result.innerText = "you win";
+    info_box_result.style.backgroundColor = "Green";
+    result.style.color = "white";
+    //total count me se aik minus kr dn
+    win_html.innerText = ++win;
+  }else{
+    result.innerText = "you lose";
+    info_box_result.style.backgroundColor = "red";
+    result.style.color = "white";
+    lose_html.innerText = ++lose;
+  }
+  checkResult();
 }
