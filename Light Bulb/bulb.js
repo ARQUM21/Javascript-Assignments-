4732db6e7ebbe68c3629912b6a060aedb6aeb703
ARQUM21 <154671211+ARQUM21@users.noreply.github.com>
2024-05-19 10:46:54 +0500

    Bulb Complete

---
+++ Light Bulb/bulb.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector('.btn');
+
+        let audio = document.querySelector('#audio');
+
+        let body = document.querySelector('body');
+
+
+
+        btn.onclick = function(){
+
+            body.classList.toggle('on');
+
+            audio.play();
+
+        }
